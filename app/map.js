0589a27b2dc9e4baf58b548ae9098ca939d06c79
Yusso <98551034+Y-Yahyaoui@users.noreply.github.com>
2022-10-29 22:52:19 +0100

    Add files via upload

---
+++ app/map.js
@@ -0,0 +1,47 @@
+var map = L.map('map', ).setView([33.977711, -6.865126], 16);
+    
+// var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+new L.basemapsSwitcher([
+    {
+      layer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map), //DEFAULT MAP
+      icon: 'SwitchBasemap/example/assets/images/img1.PNG',
+      name: 'Map one'
+    },
+    {
+      layer: L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',{
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      }),
+      icon: 'SwitchBasemap/example/assets/images/img2.PNG',
+      name: 'Map two'
+    },
+    {
+      layer: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      }),
+      icon: 'SwitchBasemap/example/assets/images/img3.PNG',
+      name: 'Map three'
+    },
+  ], { position: 'topright' }).addTo(map);
+
+style="width: 1500px; height: 800px;"
+
+
+		// create fullscreen control
+		var fsControl = L.control.fullscreen();
+		// add fullscreen control to the map
+		map.addControl(fsControl);
+
+		// detect fullscreen toggling
+		map.on('enterFullscreen', function(){
+			if(window.console) window.console.log('enterFullscreen');
+		});
+		map.on('exitFullscreen', function(){
+			if(window.console) window.console.log('exitFullscreen');
+		});
+
