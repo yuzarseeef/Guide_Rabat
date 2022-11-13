bb78e42f4b98a50a8e76b7ef8368d4bf968a2cbe
Yusso <yahyaouiyoussef@iav.ac.ma>
2022-11-13 13:07:58 +0100

    hope

--- app/map.js
+++ app/map.js
@@ -1,9 +1,4 @@
-var map = L.map('map', ).setView([33.977711, -6.865126], 16);
-    
-// var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+var map = L.map('map', ).setView([33.977711, -6.865126], 16); 
 
 new L.basemapsSwitcher([
     {
@@ -32,8 +27,71 @@ new L.basemapsSwitcher([
     },
   ], { position: 'topright' }).addTo(map);
 
-style="width: 1500px; height: 800px;"
+// style="width: 1500px; height: 800px;"
+  
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(33.9895242, -6.8484591),
+		L.latLng(33.9956475,-6.8475352),
+	],
+	createMarker: function (i, waypoints, n) {
+		var startIcon = L.icon({
+			iconUrl: 'app/dist/img/green.png',
+			shadowUrl: 'app/dist/img/shadow.png',
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
+		var sampahIcon = L.icon({
+			iconUrl: 'app/dist/img/yellow.png',
+			shadowUrl: 'app/dist/img/shadow.png',
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
+		var endIcon = L.icon({
+			iconUrl: 'app/dist/img/red.png',
+			shadowUrl: 'app/dist/img/shadow.png',
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41]
+		});
+		if (i == 0) {
+			marker_icon = startIcon
+		} else if (i > 0 && i < n - 1) {
+			marker_icon = sampahIcon
+		} else if (i == n-1) {
+			marker_icon = endIcon
+		}
+		var marker = L.marker(waypoints.latLng, {
+			draggable: true,
+			bounceOnAdd: true,
+			bounceOnAddOptions: {
+				duration: 1000,
+				height: 800,
+				function() {
+					(bindPopup(myPopup).openOn(mymap))
+				}
+			},
+			icon: marker_icon
+		});
+		return marker
+	},
+	// geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+})).addTo(map);
 
+var k = 2 ;
+
+map.on('click', function (e) {
+	control.spliceWaypoints(k, 1, e.latlng);
+	k+=1;
+});
+
+L.Routing.errorControl(control).addTo(map);
 
 		// create fullscreen control
 		var fsControl = L.control.fullscreen();
@@ -54,66 +112,3 @@ style="width: 1500px; height: 800px;"
       }
     
   }).addTo(map);
-
-  var control = L.Routing.control(L.extend(window.lrmConfig, {
-    waypoints: [
-      L.latLng(33.9895242, -6.8484591),
-      L.latLng(33.9956475,-6.8475352),
-    ],
-    createMarker: function (i, waypoints, n) {
-      var startIcon = L.icon({
-        iconUrl: 'app/dist//img/green.png',
-        shadowUrl: 'app/dist//img/shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-      });
-      var sampahIcon = L.icon({
-        iconUrl: 'app/dist//img/yellow.png',
-        shadowUrl: 'app/dist//img/shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-      });
-      var endIcon = L.icon({
-        iconUrl: 'app/dist//img/red.png',
-        shadowUrl: 'app/dist//img/shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-      });
-      if (i == 0) {
-        marker_icon = startIcon
-      } else if (i > 0 && i < n - 1) {
-        marker_icon = sampahIcon
-      } else if (i == n-1) {
-        marker_icon = endIcon
-      }
-      var marker = L.marker(waypoints.latLng, {
-        draggable: true,
-        bounceOnAdd: true,
-        bounceOnAddOptions: {
-          duration: 1000,
-          height: 800,
-          function() {
-            (bindPopup(myPopup).openOn(mymap))
-          }
-        },
-        icon: marker_icon
-      });
-      return marker
-    },
-    // geocoder: L.Control.Geocoder.nominatim(),
-    routeWhileDragging: true,
-  })).addTo(map);
-  
-  var k = 2 ;
-  
-  map.on('click', function (e) {
-    control.spliceWaypoints(k, 1, e.latlng);
-    k+=1;
-  });
-  L.Routing.errorControl(control).addTo(map);
